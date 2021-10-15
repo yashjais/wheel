cf7eaecdc73465d6a435b4a0fb68038f7b4de2ed
yashjais <yash.jaiswal@bigbinary.com>
2021-10-15 12:49:41 +0530

    added notes header

--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-import { Search, Settings, Plus } from "@bigbinary/neeto-icons";
-import { Typography } from "@bigbinary/neetoui/v2";
-import { MenuBar } from "@bigbinary/neetoui/v2/layouts";
+import { Search, Settings, Plus, BurgerMenu } from "@bigbinary/neeto-icons";
+import { Typography, Input, Button } from "@bigbinary/neetoui/v2";
+import { MenuBar, Header } from "@bigbinary/neetoui/v2/layouts";
 import EmptyNotesListImage from "images/EmptyNotesList";
-import { Button, PageLoader } from "neetoui";
-import { Header, SubHeader } from "neetoui/layouts";
+import { PageLoader } from "neetoui";
+import { SubHeader } from "neetoui/layouts";
 
 import notesApi from "apis/notes";
 import EmptyState from "components/Common/EmptyState";
@@ -15,112 +15,127 @@ import NewNotePane from "./NewNotePane";
 import NoteTable from "./NoteTable";
 
 const Notes = () => {
-  const [loading, setLoading] = useState(true);
-  const [showNewNotePane, setShowNewNotePane] = useState(false);
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedNoteIds, setSelectedNoteIds] = useState([]);
-  const [notes, setNotes] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [showNewNotePane, setShowNewNotePane] = useState(false);
+  // const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [selectedNoteIds, setSelectedNoteIds] = useState([]);
+  // const [notes, setNotes] = useState([]);
   const [isSearchCollapsed, setIsSearchCollapsed] = useState(true);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+  // useEffect(() => {
+  //   fetchNotes();
+  // }, []);
 
-  const fetchNotes = async () => {
-    try {
-      setLoading(true);
-      const response = await notesApi.fetch();
-      setNotes(response.data.notes);
-    } catch (error) {
-      logger.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const fetchNotes = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await notesApi.fetch();
+  //     setNotes(response.data.notes);
+  //   } catch (error) {
+  //     logger.error(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  if (loading) {
-    return <PageLoader />;
-  }
+  // if (loading) {
+  //   return <PageLoader />;
+  // }
+
+  const renderNotesMenubar = () => (
+    <MenuBar showMenu={true} title={<Typography style="h2">Notes</Typography>}>
+      <MenuBar.Block label="All" count={200} active />
+      <MenuBar.Block label="Users" count={80} />
+      <MenuBar.Block label="Leads" count={60} />
+      <MenuBar.Block label="Visitors" count={60} />
+      <MenuBar.SubTitle
+        iconProps={[
+          {
+            icon: () => <Search size={20} />,
+            onClick: () => setIsSearchCollapsed(!isSearchCollapsed)
+          }
+        ]}
+      >
+        <Typography
+          component="h4"
+          style="h5"
+          textTransform="uppercase"
+          weight="bold"
+        >
+          Segments
+        </Typography>
+      </MenuBar.SubTitle>
+
+      <MenuBar.Search
+        collapse={isSearchCollapsed}
+        onCollapse={() => setIsSearchCollapsed(true)}
+      />
+      <MenuBar.Block label="Europe" count={80} />
+      <MenuBar.Block label="Middle-East" count={60} />
+      <MenuBar.Block label="Asia" count={60} />
+
+      <MenuBar.SubTitle
+        iconProps={[
+          {
+            icon: () => <Search size={20} />
+          },
+
+          {
+            icon: () => <Plus size={20} />
+          },
+          {
+            icon: () => <Settings size={20} />
+          }
+        ]}
+      >
+        <Typography
+          component="h4"
+          style="h5"
+          textTransform="uppercase"
+          weight="bold"
+        >
+          Tags
+        </Typography>
+      </MenuBar.SubTitle>
+
+      <MenuBar.Search
+        collapse={isSearchCollapsed}
+        onCollapse={() => setIsSearchCollapsed(true)}
+      />
+      <MenuBar.Block label="Sales" count={80} />
+      <MenuBar.Block label="Finance" count={60} />
+      <MenuBar.Block label="User Experience" count={60} />
+    </MenuBar>
+  );
+
+  const renderNotesSection = () => (
+    <div>
+      <Header
+        actionBlock={[
+          <Input
+            prefix={<Search size={16} />}
+            placeholder="Search Name, Email, Phone Number, Etc."
+          />,
+          <Button
+            // onClick={() => setShowNewNotePane(true)}
+            style="primary"
+            label="Add Note"
+            iconPosition="right"
+            icon={() => <Plus size={18} />}
+          />
+        ]}
+        menuBarToggle={() => <BurgerMenu />}
+        title="All Notes"
+      />
+    </div>
+  );
 
   return (
     <div className="flex">
-      <MenuBar
-        showMenu={true}
-        title={<Typography style="h2">Notes</Typography>}
-      >
-        <MenuBar.Block label="All" count={200} active />
-        <MenuBar.Block label="Users" count={80} />
-        <MenuBar.Block label="Leads" count={60} />
-        <MenuBar.Block label="Visitors" count={60} />
-        <MenuBar.SubTitle
-          iconProps={[
-            {
-              icon: () => <Search size={20} />,
-              onClick: () => setIsSearchCollapsed(!isSearchCollapsed)
-            }
-          ]}
-        >
-          <Typography
-            component="h4"
-            style="h5"
-            textTransform="uppercase"
-            weight="bold"
-          >
-            Segments
-          </Typography>
-        </MenuBar.SubTitle>
-
-        <MenuBar.Search
-          collapse={isSearchCollapsed}
-          onCollapse={() => setIsSearchCollapsed(true)}
-        />
-        <MenuBar.Block label="Europe" count={80} />
-        <MenuBar.Block label="Middle-East" count={60} />
-        <MenuBar.Block label="Asia" count={60} />
-
-        <MenuBar.SubTitle
-          iconProps={[
-            {
-              icon: () => <Search size={20} />
-            },
-
-            {
-              icon: () => <Plus size={20} />
-            },
-            {
-              icon: () => <Settings size={20} />
-            }
-          ]}
-        >
-          <Typography
-            component="h4"
-            style="h5"
-            textTransform="uppercase"
-            weight="bold"
-          >
-            Tags
-          </Typography>
-        </MenuBar.SubTitle>
-
-        <MenuBar.Search
-          collapse={isSearchCollapsed}
-          onCollapse={() => setIsSearchCollapsed(true)}
-        />
-        <MenuBar.Block label="Sales" count={80} />
-        <MenuBar.Block label="Finance" count={60} />
-        <MenuBar.Block label="User Experience" count={60} />
-      </MenuBar>
-      <Header
-        title="Notes"
-        actionBlock={
-          <Button
-            onClick={() => setShowNewNotePane(true)}
-            label="Add New Note"
-            icon="ri-add-line"
-          />
-        }
-      />
+      {renderNotesMenubar()}
+      {renderNotesSection()}
+      {/*
       {notes.length ? (
         <>
           <SubHeader
@@ -160,7 +175,7 @@ const Notes = () => {
           onClose={() => setShowDeleteAlert(false)}
           refetch={fetchNotes}
         />
-      )}
+      )} */}
     </div>
   );
 };
